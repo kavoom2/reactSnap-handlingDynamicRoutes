d7c76cec7e3440ad68bd9e63f3519d59f456d4a3
kavoom2 <kavoom2@gmail.com>
2021-11-30 02:22:56 +0900

    테스트 완료

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+// * 패키지 React Snap의 index.js | run.js를 사용헙니다.
+// * 해당 스크립트 중 includes를 package.json에 의존하지 않고, React-Router에 따라 동적으로 변하도록 하였습니다.
+
+const url = require("url");
+const { run } = require("react-snap");
+const {
+  reactSnap,
+  homepage,
+  devDependencies,
+  dependencies,
+} = require(`${process.cwd()}/package.json`);
+
+const { Routes } = require("../src/Routes");
+
+const packages = require(`${process.cwd()}/package.json`);
+
+console.log("패키지는...", packages);
+console.log("ReactSnap Object is...", reactSnap);
+
+const publicUrl = process.env.PUBLIC_URL || homepage;
+
+console.log(devDependencies, dependencies);
+
+const reactScriptsVersion = parseInt(
+  (devDependencies && devDependencies["react-scripts"]) ||
+    (dependencies && dependencies["react-scripts"])
+);
+let fixWebpackChunksIssue;
+switch (reactScriptsVersion) {
+  case 1:
+    fixWebpackChunksIssue = "CRA1";
+    break;
+  case 2:
+    fixWebpackChunksIssue = "CRA2";
+    break;
+}
+
+run({
+  publicPath: publicUrl ? url.parse(publicUrl).pathname : "/",
+  fixWebpackChunksIssue,
+  ...reactSnap,
+  // ! inlcude를 Routes으로 덮어 씌우면 동적으로 출력할 수 있습니다.
+  include: Routes,
+}).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
---
+++ src/Routes.js
@@ -0,0 +1,5 @@
+// * NodeJS에서 사용되는 모듈 문법을 사용해야 합니다. (Import + Export는 ES6 문법입니다.)
+
+// ** Export: module.exports 또는 exports 를 사용해야 합니다.
+// ** Import: require("...")
+exports.Routes = ["/a", "/b", "/c", "/d"];
